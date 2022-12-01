ac0d541cc244ed60f629b84101a7b59ef4abf018
Martin Lenart <martin_lenart@icloud.com>
2022-12-01 15:37:42 +0100

    update

---
+++ Temp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Temp
+{
+    internal class Program
+    {
+
+        class Friend
+        {
+            public string FirstName { get; set; }
+            
+            public override string ToString()
+            {
+                return $"Hello {FirstName}";
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Friend f = new Friend { FirstName = "Kalle" };
+            Console.WriteLine(f.ToString());
+
+            List<object> list = new List<object>();
+            list.Add(f);
+            list.Add(new Friend { FirstName = "Mary" });
+            list.Add(4);
+            list.Add(true);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
